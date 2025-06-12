fc66e4adf7d1b44e7cd40a3c996995f908cf7186
yugal07 <yashsadhwani544@gmail.com>
2025-06-13 02:41:37 +0530

    App.jsx include random

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ import CountryExplorer from './components/CountryExplorer'
 import IPLViewer from './components/IPLViewer'
 import NASAAstronomyExplorer from './components/NASAAstronomyExplorer'
 import DailyBhajanFinder from './components/DailyBhajanFinder'
+import RandomIndianFacts from './components/RandomIndianFacts'
 
 function App() {
   return (
     // <CountryExplorer /> // Un comment to See country data
     // <IPLViewer />
     // <NASAAstronomyExplorer />
-    <DailyBhajanFinder />
+    // <DailyBhajanFinder />
+    <RandomIndianFacts />
   )
 }
 
